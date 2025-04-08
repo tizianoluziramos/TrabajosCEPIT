12b71fb37985252e0cae03720ff3a94d3085c3a0
Tiziano Tomas Luzi Ramos <tizianoluziramos@gmail.com>
2025-04-08 16:16:30 -0700

    modified:   RickyMorty/js/Modules/functions/espanol.js

--- RickyMorty/js/Modules/functions/espanol.js
+++ RickyMorty/js/Modules/functions/espanol.js
@@ -10,16 +10,24 @@ function espanol(personaje) {
     personaje.gender = "Hombre";
   } else if (personaje.gender === "Female") {
     personaje.gender = "Mujer";
+  } else if (personaje.gender === "unknown") {
+    personaje.gender = "Desconocido";
   }
   if (personaje.origin.name === "unknown") {
     personaje.origin.name = "Desconocido";
+  } else if (personaje.origin.name === "Earth (Replacement Dimension)") {
+    personaje.origin.name = "Tierra (Dimension de remplazo)";
   } else if (personaje.origin.name.startsWith("Earth")) {
     personaje.origin.name = personaje.origin.name.replace("Earth", "Tierra");
   }
   if (personaje.species === "Human") {
-    personaje.species = "Humano"
+    personaje.species = "Humano";
+  } else if (personaje.species === "Humanoid") {
+    personaje.species = "Humanoide";
+  } else if (personaje.species === "unknown") {
+    personaje.species = "Desconocido";
   }
   return personaje;
 }
 
-export { espanol }
+export { espanol };

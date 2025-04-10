24ad391035eea8ef3ad8098db4b433b30e9706fb
Tiziano Tomas Luzi Ramos <tizianoluziramos@gmail.com>
2025-04-10 07:13:27 -0700

    modified:   RickyMorty/js/Modules/functions/modal/mostrarDetallePersonaje.js deleted:    RickyMorty/js/Modules/functions/modal/obtenerDetallePersonaje.js

--- RickyMorty/js/Modules/functions/modal/mostrarDetallePersonaje.js
+++ RickyMorty/js/Modules/functions/modal/mostrarDetallePersonaje.js
@@ -1,5 +1,17 @@
-import { obtenerDetallePersonaje } from "./obtenerDetallePersonaje.js";
 import { crearVentanaEmergente } from "./crearVentanaEmergente.js";
+import { espanol } from "../generartexto/espanol.js";
+
+async function obtenerDetallePersonaje(id) {
+  try {
+    const response = await fetch(
+      `https://rickandmortyapi.com/api/character/${id}`
+    );
+    const data = await response.json();
+    return espanol(data);
+  } catch (error) {
+    console.error("Error al obtener datos del personaje:", error);
+  }
+}
 
 export async function mostrarDetallePersonaje(id) {
   const personaje = await obtenerDetallePersonaje(id);
